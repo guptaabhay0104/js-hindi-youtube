24155edc7f956f2f6bc4706468bd898b5760f6a0
Abhay Gupta <159725724+guptaabhay0104@users.noreply.github.com>
2024-05-10 19:28:59 +0000

    changes done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3 ) //we are using nodejs, not browser
+
+//Data Types
+
+/* 1. number
+2. undefined -> types is undefined only
+3. string -> collection of characters. Represented within double quotes.
+4. null -> standalone value. It is of 'object' type.
+5. boolean -> true/false
+6. bigint -> used for very large numbers
+
+To check the type of any value in JS, use 'typeof' keyword.
+Ex- console.log(typeof "Abhay")
+*/
