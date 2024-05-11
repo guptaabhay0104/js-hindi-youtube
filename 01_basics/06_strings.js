54156a6e4b8a9e2bb57e747a8cbeb15d3e2d7d46
Abhay Gupta <159725724+guptaabhay0104@users.noreply.github.com>
2024-05-11 22:01:14 +0000

    string and its methods

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,18 @@
+const myName = "Abhay Gupta";
+const age = 23;
+
+console.log(`Hello...My name is ${myName} and I am ${age} years old`);
+
+const myEmail = new String(' abhaygt1804@gmail.com      ')
+
+//String methods
+
+console.log(myEmail.toUpperCase());
+console.log(myEmail.substring(2, 7));
+console.log(myEmail.slice(-1, 12));
+console.log(myEmail.charAt(3));
+console.log(myEmail.charCodeAt(5));
+console.log(myEmail.indexOf('t'));
+console.log(myEmail.includes('abh'));
+console.log(myEmail.trim());
+console.log(myEmail.split('.'));
